756a5ae723848ec7be42bb6c25b5c3d621a8a016
Bilokon Yaroslav <uvill.github@gmail.com>
2025-02-01 19:17:42 +0200

    fix hasband

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -8,7 +8,7 @@ export const Person = ({ person }) => (
     )}
     {person.isMarried ? (
       <p className="Person__partner">
-        {person.partnerName} is my {person.sex === 'm' ? 'wife' : 'husnand'}
+        {person.partnerName} is my {person.sex === 'm' ? 'wife' : 'husband'}
       </p>
     ) : (
       <p className="Person__partner">I am not married</p>
